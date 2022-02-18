7e560b0467c3f37d777af5657673870324fc5b38
Jean-Charles LAYOUN <jean-charles.layoun@polytechnique.edu>
2022-02-18 18:00:36 -0500

    Changed JuMP model to respond to comments made on the pull request.

--- lab2/JuMP_BIGGSEXP6.jl
+++ lab2/JuMP_BIGGSEXP6.jl
@@ -1,8 +1,10 @@
 using JuMP, Ipopt
 
 function create_JuMP_BiggsEXP6()
-	model = Model(with_optimizer(Ipopt.Optimizer))
-	@variable(model, x[i=1:6])
+	z = [i/10 for i in 1:13]
+	y = [exp(-zi) - 5*exp(-10*zi) + 3*exp(-4*zi) for zi in z]
+	model = Model()
+	@variable(model, x[i=1:6]. start=ones(6))
 	@NLobjective(model, Min, sum(x[3]*exp(-x[1]*z[i]) - x[4]*exp(-x[2]*z[i]) + x[6]*exp(-x[5]*z[i]) - y[i] for i in 1:13))
 	return model
 end
