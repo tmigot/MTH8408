cd9138794c8b17566c18be491951772301beafbe
Hugoatlas <97746957+Hugoatlas@users.noreply.github.com>
2023-02-06 09:55:49 -0500

    Update branch for rebase

--- lab1/Exercice3.jl
+++ lab1/Exercice3.jl
@@ -1,29 +1,50 @@
 
+write(stdin.buffer, 0x0C);
+
 function f(x; alpha=1)
     return x^alpha
 end
 
 N=20;
-M=zeros((N+1,N+1));
+x=1;
 
-M[1,1] = 1;
+A=zeros((N,1));
+for i=1:N
+    A[i,1]=i;
+end
+
+
+Basis=zeros((N,1));
+for i=1:N
+    Basis[i,1]=exp(A[i,1]*x);
+end
+
+
+M=zeros((N,N));
 for i=1:N
     for j=1:N
-        M[i+1,j+1]=i^(j-1);
+        M[i,j]=(A[i,1])^(j-1);
     end
 end
 
+display(M*inv(M))
 
-D=zeros((N+1,N+1));
-k=0
-for i=0.:N
-    D[i+1,i+1]=f(k);
-    k+=1
+D=zeros((N,N));
+for i=1:N
+    D[i,i]=f(A[i,1]);
 end
 
 
-B=zeros((N+1,1));
-B[2,1]=1;
+B=zeros((N,1));
+for i=1:N
+    B[i,1]=1/(factorial(i))
+end
+# B[2,1]=1;
 
 
-B_tranform = M*D*inv(M)*B
+B_tranform = M*D*inv(M)*B;
+
+display(B_tranform)
+
+
+print(sum(B_tranform))
